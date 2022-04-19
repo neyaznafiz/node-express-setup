1ba6976be5f3db0c129d7cc6ff781810a6b0e143
niiyazobug <niiyaz5555@gmail.com>
2022-04-20 03:31:50 +0600

    install nodemon and setup in pachage.json file

--- _steps.js
+++ _steps.js
@@ -12,6 +12,14 @@
  * 3. declare port
  * 4. set app.get('/')
  * 5. listen to port
- * 6. node index.js
+//  * 6. node index.js
  * 7. check your browser for the port
+ * 
+ * ---------------------------
+ * For Server Auto ReStart
+ * ---------------------------
+ * 1. npm install -g nodemon
+ * 2. in package.json file set those code in script -  "start": "node index.js",
+                                                       "start-dev": "nodemon index.js", 
+ * 3. nodemon index.js   
 */
